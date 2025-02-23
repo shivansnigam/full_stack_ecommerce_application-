39448c09488de0e8d7b02bf96d8b252519b935cb
shivansnigam <142572162+shivansnigam@users.noreply.github.com>
2025-02-24 00:17:03 +0530

    Updated backend

--- server/server.js
+++ server/server.js
@@ -3,45 +3,50 @@ import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
-import authRoutes from "./routes/authRoute.js"
-import categoryRoutes from "./routes/categoryRoutes.js"
-import productRoutes from "./routes/productRoutes.js"
-import cors from "cors"
+import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
 
+const app = express();
 
+app.use(
+    cors({
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+    })
+  );
 
 //configure env
 dotenv.config();
 
-
 //databse config
 connectDB();
 
-const app = express();
+
+
+
 
 //middelwares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-
-
 // routes
-app.use("/api/v1/auth",authRoutes)
-app.use("/api/v1/category",categoryRoutes )
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-
+// Default route for checking API status
 app.get("/", (req, res) => {
-    res.send({
-        message: "welcome to ecommerce website"
-    });
+  res.send("API is running...");
 });
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-    console.log(
-      `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
-        .white
-    );
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
+  );
 });
